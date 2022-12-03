47642284a61cc9310998d48e05a092e89b3d7114
Ko-Eunseo <koyk0408@naver.com>
2022-12-03 19:35:40 +0900

    모달 슬라이스 이름 변경

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import modalReducer from '../redux/modalSlice';
+import modalSlice from '../redux/modalSlice';
 
 export const store = configureStore({
   reducer: {
     //여기서 리듀서를 관리한다.
-    modal: modalReducer,
+    modal: modalSlice,
   },
 });
